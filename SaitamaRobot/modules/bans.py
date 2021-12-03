01a85efa519080345228cf3540597f2721ebec43
Leviathanos <60214480+Leviathanos@users.noreply.github.com>
2021-12-03 03:01:43 -0500

    Update bans.py

--- SaitamaRobot/modules/bans.py
+++ SaitamaRobot/modules/bans.py
@@ -386,7 +386,7 @@ __help__ = """
 
 BAN_HANDLER = CommandHandler("ban", ban)
 TEMPBAN_HANDLER = CommandHandler(["tban"], temp_ban)
-SNAP_HANDLER = CommandHandler("snap", snap)
+BOOP_HANDLER = CommandHandler("boop", boop)
 UNBAN_HANDLER = CommandHandler("unban", unban)
 ROAR_HANDLER = CommandHandler("roar", selfunban)
 BOOPME_HANDLER = DisableAbleCommandHandler(
