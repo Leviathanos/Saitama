c40ed57fe9ce3163714406f36e3ed93b9e4c4a8f
Leviathanos <60214480+Leviathanos@users.noreply.github.com>
2021-12-10 22:01:55 -0500

    Update animechan.py

--- SaitamaRobot/modules/animechan.py
+++ SaitamaRobot/modules/animechan.py
@@ -1,5 +1,5 @@
 import requests
-from SaitamaRobot import bot
+from SaitamaRobot import
 from pyrogram import filters
 from pyrogram.types.bots_and_keyboards.inline_keyboard_button import InlineKeyboardButton
 from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeyboardMarkup
